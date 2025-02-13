4097fb40f7a3c5e2c467e80024161057176db022
chw3021 <ckgusdnr1205@gmail.com>
2025-02-13 17:15:26 +0900

    250213

---
+++ MusicoveryProject/src/main/java/com/musicovery/MusicoveryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.musicovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicoveryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicoveryProjectApplication.class, args);
+	}
+
+}
---
+++ MusicoveryProject/src/test/java/com/musicovery/MusicoveryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicoveryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
