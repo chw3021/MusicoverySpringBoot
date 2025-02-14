faa51f7c4be448998375eba8dda5d3d088cde8f7
chw3021 <ckgusdnr1205@gmail.com>
2025-02-14 17:07:14 +0900

    250214

--- MusicoveryProject/src/main/java/com/musicovery/MusicoveryProjectApplication.java
+++ MusicoveryProject/src/main/java/com/musicovery/MusicoveryProjectApplication.java
@@ -2,8 +2,9 @@ package com.musicovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "com.musicovery")
+@SpringBootApplication(scanBasePackages = "com.musicovery", exclude = SecurityAutoConfiguration.class)
 public class MusicoveryProjectApplication {
 
 	public static void main(String[] args) {
