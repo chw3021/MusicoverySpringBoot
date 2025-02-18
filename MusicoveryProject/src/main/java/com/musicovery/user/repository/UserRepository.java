98d366f19991d02d368c4fd0b5f2d1c95df6e4cf
Stophun00 <grandj99@gmail.com>
2025-02-18 11:59:14 +0900

    20250218

---
+++ MusicoveryProject/src/main/java/com/musicovery/user/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.musicovery.user.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.musicovery.user.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByEmail(String email); // 이메일로 회원 찾기
+
+	boolean existsByEmail(String email); // 이메일 중복 확인
+
+	boolean existsByNickname(String nickname); // 닉네임 중복 확인
+}
---
+++ MusicoveryProject/src/main/java/com/musicovery/user/service/UserServiceImpl.java
@@ -0,0 +1,50 @@
+package com.musicovery.user.service;
+
+import java.util.Optional;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.musicovery.user.dto.UserDTO;
+import com.musicovery.user.dto.UserSignupDTO;
+import com.musicovery.user.entity.User;
+import com.musicovery.user.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+	private final UserRepository userRepository = null;
+	private final PasswordEncoder passwordEncoder = null;
+
+	@Override
+	public User SignupUser(UserSignupDTO userSignupDTO) {
+		if (userRepository.existsByEmail(userSignupDTO.getEmail())) {
+			throw new IllegalStateException("이미 존재하는 이메일입니다.");
+		}
+
+		User user = User.builder()
+				.email(userSignupDTO.getEmail())
+				.passwd(passwordEncoder.encode(userSignupDTO.getPasswd())) // 비밀번호 암호화
+				.nickname(userSignupDTO.getNickname())
+				.phone(userSignupDTO.getPhone())
+				.address(userSignupDTO.getAddress())
+				.isActive(true).build();
+
+		return userRepository.save(user);
+	}
+
+	@Override
+	public User login(UserDTO userDTO) {
+		Optional<User> optionalUser = userRepository.findByEmail(userDTO.getEmail());
+
+		if (optionalUser.isEmpty()) {
+			throw new IllegalArgumentException("이메일이 존재하지 않습니다.");
+		}
+
+		User user = optionalUser.get();
+		if (!passwordEncoder.matches(userDTO.getPasswd(), user.getPasswd())) {
+			throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+		}
+
+		return user; // JWT 토큰 발급 로직 추가 가능
+	}
+}
