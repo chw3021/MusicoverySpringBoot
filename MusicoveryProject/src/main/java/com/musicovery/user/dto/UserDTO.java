5dcd0b2907020144adcfed20c6cbfd7ca42ccf6a
Stophun00 <grandj99@gmail.com>
2025-02-20 15:27:17 +0900

    250220

--- MusicoveryProject/src/main/java/com/musicovery/user/dto/UserDTO.java
+++ MusicoveryProject/src/main/java/com/musicovery/user/dto/UserDTO.java
@@ -1,5 +1,7 @@
 package com.musicovery.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,11 +12,13 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDTO {
-	
+
 	private String userId;
-	
+
 	private String email;
-	
+
+	// JSON 응답에서 제외 (보안 목적)
+	@JsonIgnore
 	private String passwd;
-	
+
 }
