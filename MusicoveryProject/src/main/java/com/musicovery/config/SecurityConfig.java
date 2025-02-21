bc52f436bf0a3c67628367060e6d715ff7e85145
chw3021 <ckgusdnr1205@gmail.com>
2025-02-21 09:29:20 +0900

    250221

--- MusicoveryProject/src/main/java/com/musicovery/config/SecurityConfig.java
+++ MusicoveryProject/src/main/java/com/musicovery/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,15 +22,26 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-	        .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화 (필요에 따라 활성화)
-	        .authorizeHttpRequests(authorize -> authorize
-	            .requestMatchers("/**").permitAll() // 인증 없이 접근 가능
-	            .anyRequest().authenticated()
-	        )
-	        .formLogin(form -> form.disable()) // 폼 로그인 비활성화
-	        .httpBasic(httpBasic -> httpBasic.disable()); // HTTP Basic 인증 비활성화
-
+            .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화 (필요에 따라 활성화)
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/", "/login", "/oauth2/**").permitAll() // 인증 없이 접근 가능
+                .anyRequest().authenticated()
+            )
+            .oauth2Login(oauth2Login -> oauth2Login
+                .loginPage("/login")
+                .defaultSuccessUrl("/home", true)
+                .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
+                    .oidcUserService(this.oidcUserService())
+                )
+            )
+            .formLogin(form -> form.disable()) // 폼 로그인 비활성화
+            .httpBasic(httpBasic -> httpBasic.disable()); // HTTP Basic 인증 비활성화
 
         return http.build();
+    }
+
+    @Bean
+    public OidcUserService oidcUserService() {
+        return new OidcUserService();
     }
 }
