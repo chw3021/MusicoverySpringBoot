2d055c34e0204fde06aba35af3fa1e4ba08c1231
chw3021 <ckgusdnr1205@gmail.com>
2025-02-24 17:45:47 +0900

    250224

--- MusicoveryProject/src/main/java/com/musicovery/config/CorsConfig.java
+++ MusicoveryProject/src/main/java/com/musicovery/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // React 서버 주소
+        		.allowedOrigins("http://localhost:3000")
                 .allowCredentials(true) // 쿠키 허용
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
