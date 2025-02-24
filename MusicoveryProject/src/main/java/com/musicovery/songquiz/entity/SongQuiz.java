f9490ee5ab40ae46883e5b08ce1a57642a81ac36
excel8057 <excel8057@naver.com>
2025-02-24 18:56:02 +0900

    23535674567

--- MusicoveryProject/src/main/java/com/musicovery/songquiz/entity/SongQuiz.java
+++ MusicoveryProject/src/main/java/com/musicovery/songquiz/entity/SongQuiz.java
@@ -1,6 +1,5 @@
 package com.musicovery.songquiz.entity;
 
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table
 public class SongQuiz {
     private String artist;
     private String title;
--- MusicoveryProject/src/main/java/com/musicovery/songquiz/repository/SongQuizRepository.java
+++ MusicoveryProject/src/main/java/com/musicovery/songquiz/repository/SongQuizRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.musicovery.songquiz.entity.SongQuiz;
 
 public interface SongQuizRepository extends JpaRepository<SongQuiz, Long> {
-    List<SongQuiz> findByUserId(Long userId);
+    List<SongQuiz> findByUserId(String userId);
 }
